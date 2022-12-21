2bbf85a4a6a1e50372242132e7c4d3916831ec1f
Johannes Radmer <hannesradmer@gmail.com>
2022-12-21 08:25:44 +0100

    Moved regex to separate file.

--- src/code_lens_providers.ts
+++ src/code_lens_providers.ts
@@ -1,23 +1,20 @@
 import * as vscode from 'vscode';
-import { Card } from './card';
+import { lineSplitterRegExp, searchLineRegExp } from './regular_expressions';
 
 export class CardSearchLensProvider implements vscode.CodeLensProvider {
-    lineRegexp: RegExp = /^\/\/ *Search: *(.*?) *$/i;
-    lineSplitterRegexp: RegExp = /\r?\n/g;
-    
-    constructor() {}
+    constructor() { }
 
     onDidChangeCodeLenses?: vscode.Event<void> | undefined;
-    provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken):  vscode.ProviderResult<vscode.CodeLens[]> {
-        const lines: string[] = document.getText().split(this.lineSplitterRegexp);
-        
+    provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeLens[]> {
+        const lines: string[] = document.getText().split(lineSplitterRegExp);
+
         let codeLenses: vscode.CodeLens[] = [];
         for (const [lineNum, line] of lines.entries()) {
-            const search = this.lineRegexp.exec(line);
+            const search = searchLineRegExp.exec(line);
             if (!search || search.length !== 2) {
                 continue;
             }
-            
+
             let searchStr: string = search[1].trim();
             const cmd: vscode.Command = {
                 command: 'mtg-code.search-cards',
--- src/regular_expressions.ts
+++ src/regular_expressions.ts
@@ -1,2 +1,4 @@
 export const cardLineRegExp: RegExp = /^(\d+) +(.+)$/;
 export const lineSplitterRegExp: RegExp = /\r?\n/g;
+export const searchLineRegExp: RegExp = /^\/\/ *Search: *(.*?) *$/i;
+export const commentLineRegExp: RegExp = /^\/\/.*$/; 
